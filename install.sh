853ff587c7a1aacb1d14b27ffb5c14eae607c44f
nvol <koka81@gmail.com>
2017-07-19 04:34:24 +0300

    just run install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/tswr/.vimrc.git ~/vim-tswr/
-git clone https://github.com/gmarik/Vundle.vim.git ~/vim-tswr/.vim/bundle/Vundle.vim
-ln -Fs ~/vim-tswr/.vim ~
-ln -fs ~/vim-tswr/.vimrc ~
+#git clone https://github.com/tswr/.vimrc.git ~/vim-tswr/
+git clone https://github.com/gmarik/Vundle.vim.git ~/vimchik/.vim/bundle/Vundle.vim
+ln -Fs ~/vimchik/.vim ~
+ln -fs ~/vimchik/.vimrc ~
 vim +PluginInstall +qall
